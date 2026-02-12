6e9bc1c184c4417ae2023185f4a29256a683cd00
Shahed420 <57837102+Shahed420@users.noreply.github.com>
2026-02-11 21:25:48 -0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,10 +4,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_vendor_boot.mk
+    $(LOCAL_DIR)/twrp_X6725B.mk
 
 COMMON_LUNCH_CHOICES := \
     omni_vendor_boot-user \
     omni_vendor_boot-userdebug \
-    omni_vendor_boot-eng
+    twrp_X6725B-eng
+
 
