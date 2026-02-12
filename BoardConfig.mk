b2e3a53f2b3a150928ab2c68f94d87003e9597a0
Shahed420 <57837102+Shahed420@users.noreply.github.com>
2026-02-11 22:40:06 -0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,15 +9,22 @@ DEVICE_PATH := device/generic/vendor_boot
 ALLOW_MISSING_DEPENDENCIES := true
 
 # Architecture
-TARGET_ARCH := arm64-v8a
-TARGET_ARCH_VARIANT := generic
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 := 
 TARGET_CPU_VARIANT := generic
 TARGET_CPU_VARIANT_RUNTIME := generic
-# Bootloader
-TARGET_BOOTLOADER_BOARD_NAME := vendor_boot
-TARGET_NO_BOOTLOADER := true
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := generic
+
+TARGET_BOARD_SUFFIX := _64
+TARGET_USES_64_BIT_BINDER := true
 
 # Display
 TARGET_SCREEN_DENSITY := 480
@@ -77,5 +84,6 @@ TARGET_RECOVERY_FSTAB := device/generic/X6725B/recovery.fstab
 
 # Partition
 BOARD_FLASH_BLOCK_SIZE := 131072
+
 
 
