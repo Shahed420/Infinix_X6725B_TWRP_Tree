6d0429b1f9ef381fa2c9081a8894c3f2746953e8
Shahed420 <57837102+Shahed420@users.noreply.github.com>
2026-02-13 09:41:37 -0800

    Update twrp_X6725B.mk

--- twrp_X6725B.mk
+++ twrp_X6725B.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from X6725B device
-$(call inherit-product, device/generic/X6725B/device.mk)
+$(call inherit-product, device/infinix/X6725B/device.mk)
 
 PRODUCT_DEVICE := X6725B
 PRODUCT_NAME := twrp_X6725B
@@ -11,6 +11,7 @@ PRODUCT_MODEL := Infinix Smart 10 Plus
 PRODUCT_MANUFACTURER := Infinix
 
 PRODUCT_GMS_CLIENTID_BASE := android-infinix
+
 
 
 
