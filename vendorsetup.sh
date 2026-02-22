467a987f825a6a94a907cd12c170145d3f4b4163
Shahed420 <57837102+Shahed420@users.noreply.github.com>
2026-02-22 22:51:23 +0600

    Add files via upload

---
+++ vendorsetup.sh
@@ -0,0 +1,15 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+add_lunch_combo twrp_X6725B-userdebug
+add_lunch_combo twrp_X6725B-eng
+
+PRODUCT_DEVICE := X6725B
+PRODUCT_NAME := twrp_X6725B
+PRODUCT_BRAND := Infinix
+PRODUCT_MODEL := Infinix X6725B
+PRODUCT_MANUFACTURER := infinix
+
+
