eaffd1a7b95f8540b816c9363433dcdaeca1a887
Shahed420 <57837102+Shahed420@users.noreply.github.com>
2026-02-24 14:42:48 +0600

    Update twrp_X6725B.mk

--- twrp_X6725B.mk
+++ twrp_X6725B.mk
@@ -1,6 +1,5 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64bit.mk)
 
 # বিল্ড সিস্টেমকে ভেন্ডর বুট তৈরির নির্দেশ দিন
 PRODUCT_BUILD_VENDOR_BOOT_IMAGE := true
@@ -13,6 +12,7 @@ PRODUCT_MODEL := Infinix Smart 10 Plus
 PRODUCT_MANUFACTURER := Infinix
 
 PRODUCT_GMS_CLIENTID_BASE := android-infinix
+
 
 
 
